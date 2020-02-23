9bd0f9bd86c83be273ef318df0765690e1dfa132
jesusmaldonado <jesusmaldonado317@gmail.com>
2020-02-23 18:40:57 +0100

    Updates

---
+++ precache-manifest.6c2e5ffa2a221a6c240b81cb5c51385b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a8ebfe5bb9ddbf400c0ed2cf6ed7514",
+    "url": "/react-hooks-todo/index.html"
+  },
+  {
+    "revision": "c2c44670ead2fb7a023c",
+    "url": "/react-hooks-todo/static/css/main.257036ea.chunk.css"
+  },
+  {
+    "revision": "7d5caba9339c8e21d633",
+    "url": "/react-hooks-todo/static/js/2.5f4bd23d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-hooks-todo/static/js/2.5f4bd23d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2c44670ead2fb7a023c",
+    "url": "/react-hooks-todo/static/js/main.2550ef17.chunk.js"
+  },
+  {
+    "revision": "4b9dfafafba5e05e10b0",
+    "url": "/react-hooks-todo/static/js/runtime-main.30335d1a.js"
+  }
+]);
